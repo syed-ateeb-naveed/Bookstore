efb66e958ff02ef85e60631087e525a8bab73b8b
ateeb <ateebnaveed1996@gmail.com>
2023-12-18 23:15:35 +0500

    Default set for datetime

--- bookstore/bookapp/models.py
+++ bookstore/bookapp/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from datetime import datetime, date
 
 # Create your models here.
 class Request(models.Model):
     title = models.CharField(max_length = 200)
     author = models.CharField(max_length = 100)
-    date = models.DateField()
+    date = models.DateField(default = date.today())
     comment = models.CharField(max_length = 1000)
