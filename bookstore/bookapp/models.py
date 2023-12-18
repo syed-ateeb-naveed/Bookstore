23d9ed4ba7f7a332bba455e2f4da46771018b9cb
ateeb <ateebnaveed1996@gmail.com>
2023-12-18 23:40:43 +0500

    use django.utils for date field in Request table

--- bookstore/bookapp/models.py
+++ bookstore/bookapp/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from datetime import datetime, date
+from django.utils import timezone
 
 # Create your models here.
 class Request(models.Model):
     title = models.CharField(max_length = 200)
     author = models.CharField(max_length = 100)
-    date = models.DateField(default = date.today())
+    date = models.DateTimeField(default = timezone.now().date())
     comment = models.CharField(max_length = 1000)
