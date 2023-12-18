cd9d9049917bc0a92838742c1939c9c0cac5dd84
ateeb <ateebnaveed1996@gmail.com>
2023-12-18 23:37:13 +0500

    Added templates dir in settings.py and made request.html

--- bookstore/bookstore/settings.py
+++ bookstore/bookstore/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'bookstore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['bookapp/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
