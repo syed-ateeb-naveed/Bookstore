86d0c5da589c8949dc35498f2045ccba82740c37
ateeb <ateebnaveed1996@gmail.com>
2023-12-18 23:46:42 +0500

    Modified for date only

--- bookstore/bookapp/models.py
+++ bookstore/bookapp/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-# from datetime import datetime, date
+from datetime import datetime, date
 from django.utils import timezone
 
 # Create your models here.
 class Request(models.Model):
     title = models.CharField(max_length = 200)
     author = models.CharField(max_length = 100)
-    date = models.DateTimeField(default = timezone.now)
+    date = models.DateField(default = date.today())
     comment = models.CharField(max_length = 1000)
