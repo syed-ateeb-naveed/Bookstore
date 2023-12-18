999a11bf2a577ee462d47b2177dc7db2b238778b
ateeb <ateebnaveed1996@gmail.com>
2023-12-18 23:30:16 +0500

    Create function for Request form

--- bookstore/bookapp/views.py
+++ bookstore/bookapp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .forms import RequestForm
+from .models import Request
 
 # Create your views here.
+
+def makeRequest(request):
+    form = RequestForm()
+    if request.method == "POST":
+        form = RequestForm(request.POST)
+        if form.is_valid():
+            form.save()
+    
+    context = {"form" : form}
+
+    return render(request, "request.html", context)
